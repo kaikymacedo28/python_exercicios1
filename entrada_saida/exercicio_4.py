f9cdacee3219a2447341982464b63ae80b28cf02
kaikymacedo28 <kaiky.b.macedo@gmail.com>
2024-05-07 11:20:07 -0300

    exercicio4.py

---
+++ entrada_saida/exercicio_4.py
@@ -0,0 +1,5 @@
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+nota_final = (nota1+nota2+nota3)/3
+print(f"Sua nota final é{nota_final:.02f}")
