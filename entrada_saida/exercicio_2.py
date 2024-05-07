c5ed28753e9d861d2a04912a6443982192b06d4b
kaikymacedo28 <kaiky.b.macedo@gmail.com>
2024-05-07 10:34:26 -0300

    exercicio2.py

---
+++ entrada_saida/exercicio_2.py
@@ -0,0 +1,3 @@
+estatura = int(input("Insira sua estatura em metros: "))
+conversao = estatura * 100
+print(f"A conversão de sua estatura em metros para centimetros são de: {conversao}" " centimetros")
